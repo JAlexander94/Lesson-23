5c7040f6210a612be2f5fa6083ab7bea0116735b
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-02-01 19:54:34 +0000

    exercise 2&3 done

--- activities/03-Stu-MovieButtonLayout/starter/script.js
+++ activities/03-Stu-MovieButtonLayout/starter/script.js
@@ -4,14 +4,27 @@
  // Function for displaying movie data
  function renderButtons() {
 
+  $("#buttons-view").empty()
+
    // YOUR CODE GOES HERE
+   for (i=0;i<movies.length;i++){
+    moviebtn = $("<button></button>")
+    moviebtn.text(movies[i])
+    moviebtn.addClass("movie")
+    moviebtn.attr("data-movie",movies[i])
+    $("#buttons-view").append(moviebtn)
+   }
 
  }
 
  // This function handles events where one button is clicked
- $("#add-movie").on("click", function() {
+ $("#add-movie").on("click", function(event) {
 
    // YOUR CODE GOES HERE
+   event.preventDefault()
+   const newmovie = $('#movie-input').val().trim()
+   movies.push(newmovie)
+   renderButtons()
 
  });
 
